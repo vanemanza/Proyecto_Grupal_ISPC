cb6b476979908f120c1f77f6944d325c8dcb0519
mariacuenca <marcuencac@outlook.com>
2022-08-13 16:30:59 -0300

    trabajoFinal

---
+++ trabajopractico.py
@@ -0,0 +1,8 @@
+# Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
+# Juan,  Mario,  Ariel,  Josefina,  Marianella.
+# Ordenarla alfabéticamente y mostrarla por pantalla.
+
+clientes = ['Juan','Mario','Ariel','Josefina','Marianella']
+print (clientes)
+clientes.sort()
+print (clientes)
